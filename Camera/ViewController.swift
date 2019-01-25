f4617a681d3a9378fdb5319370aa7550b25f9cee
Siddharth <sathesiddharth14@gmail.com>
2019-01-25 14:08:26 +0530

    Camera working fine, No buttons working yet.

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let cameraController = CameraController()
+    
+    @IBOutlet fileprivate var capturePreviewView: UIView!
+    
+    @IBOutlet fileprivate var photoModeButton: UIButton!
+    
+    @IBOutlet fileprivate var toggleFlashButton: UIButton!
+    
+    @IBOutlet fileprivate var toggleCameraButton: UIButton!
+    
+    @IBOutlet fileprivate var videoModeButton: UIButton!
+    
+
+    @IBOutlet weak var captureButton: UIButton!
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+extension ViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        configureCameraController()
     }
 
-
+    func configureCameraController() {
+        cameraController.prepare { (error) in
+            if let error = error {
+                print(error)
+            }
+            try? self.cameraController.displayPreview(on: self.capturePreviewView)
+        }
+    }
 }
 
